f033a3635e8bbba91e52201db836e9c9c6405844
Hevagog <qkawiak@gmail.com>
2026-06-22 12:55:26 +0200

    delete not used commformerhm

--- src/agents/commformerhm/__init__.py
+++
@@ -1,7 +0,0 @@
-from agents.commformerhm.commformerhm_mappo import CommFormerHMMAPPO
-from agents.commformerhm.commformerhm_runner import CommFormerHMRunner
-
-__all__ = [
-    "CommFormerHMMAPPO",
-    "CommFormerHMRunner",
-]
